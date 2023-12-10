c3fb3450026596ac2a988233e0c9d754cf1acc8a
wkktoria <127852624+wkktoria@users.noreply.github.com>
2023-12-10 20:37:29 +0100

    Read status code when ran without arguments

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
-use std::env;
+use std::{env, io};
 
 use colored::Colorize;
 use http_status_codes::display_info;
 
 fn main() {
-    for status_code in env::args().skip(1) {
+    let args = env::args();
+    if args.len() == 1 {
+        let mut status_code = String::new();
+
+        println!("\nStatus code:");
+        io::stdin()
+            .read_line(&mut status_code)
+            .expect(format!("{}", "Failed to read status code.".red()).as_str());
+
         display_info(&status_code);
+    } else {
+        for status_code in args.skip(1) {
+            display_info(&status_code);
+        }
     }
 }
