8c3835f2c6df3346731cbcbe2a46df0bd04d7171
wkktoria <127852624+wkktoria@users.noreply.github.com>
2023-12-10 20:31:41 +0100

    Get few status codes

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,7 @@ use colored::Colorize;
 use http_status_codes::display_info;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    if args.len() == 2 {
-        let status_code = &args[1];
-        display_info(status_code);
-    } else {
-        println!("{}", "\nPass status code as an argument!".red());
+    for status_code in env::args().skip(1) {
+        display_info(&status_code);
     }
 }
